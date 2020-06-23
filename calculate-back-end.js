3dbe15552630ea93b43357cad6b7b484694ea942
yiguo-17 <yiguo.qu@codeimmersives.com>
2020-06-22 20:45:27 -0400

    edit back-end file based on test file.

--- calculate-back-end.js
+++ calculate-back-end.js
@@ -1,7 +1,16 @@
 /******************
  * YOUR CODE HERE *
  ******************/
-
+function calculate(str1, str2, str3){
+  const num1 = parseInt(str1);
+  const num2 = parseInt(str2);
+  if(str3 === '+'|| str3 === 'plus'|| str3 === 'added to'){return num1 + num2;}
+  else if(str3 === '-'|| str3 === 'minus'|| str3 === 'subtracted from'){return num1 - num2;}
+  else if(str3 === 'x'|| str3 === 'X'|| str3 === 'times'|| str3 === 'multiplied by'){return num1 * num2;}
+  else if(str3 === '/'|| str3 === 'divided by'){return num1 / num2;}
+  else if(str3 === '%'||str3 === 'modulus'|| str3 === 'mod'){return num1 % num2;}
+  else {return "Sorry, that's not a mathematical operation!";}
+}
 
  
 
